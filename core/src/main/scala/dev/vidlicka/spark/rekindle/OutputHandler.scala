5db7933b8233c117d24f6872a6d3e6458ff84ad6
Pavol Vidlička <pavol.vidlicka@gmail.com>
2022-03-26 20:33:39 +0100

    fixup! Implement rudimentary CLI app

--- core/src/main/scala/dev/vidlicka/spark/rekindle/OutputHandler.scala
+++ core/src/main/scala/dev/vidlicka/spark/rekindle/OutputHandler.scala
@@ -1,7 +1,6 @@
 package dev.vidlicka.spark.rekindle
 
-import fs2.Stream
-import fs2.Pipe
+import fs2.{ Pipe, Stream }
 
 /** this handles pushing the results of replayers somewhere idea:
   *   - print to stdout
